b312af1ef7913cd09e3632e2f09f37ac486493de
Marco Milani <marco.milani@giuneco.it>
2016-08-28 23:46:14 +0200

    WIP

---
+++ HelloMvb.ModelBinders/HelloMvb.App.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HelloMvb.ModelBinders;
+
+namespace HelloMvb.App.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HelloWorldModelBinder _binder;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this._binder = new HelloWorldModelBinder();
+            this.InitBinderActions();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            this._binder.SayHelloto(this.TextBox.Text);
+        }
+
+        private void InitBinderActions()
+        {
+            this._binder.Binder.AddAction<HelloWorldModelBinder>(b=> b.OutPut, () =>
+            {
+                this.OutPut.Text = this._binder.OutPut;
+            });
+        }
+    }
+}
---
+++ HelloMvb.ModelBinders/HelloMvb.ModelBinders/HelloWorldModelBinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Mvb.Core.Base;
+
+namespace HelloMvb.ModelBinders
+{
+    public class HelloWorldModelBinder : MvbBase
+    {
+        public HelloWorldModelBinder()
+        {
+            this.InitBinder();
+        }
+
+        private string _outPut;
+
+        public string OutPut
+        {
+            get { return this._outPut; }
+            set { this.SetProperty(ref this._outPut, value); }
+        }
+
+        public void SayHelloto(string name)
+        {
+            this.HelloWorker(name).ConfigureAwait(false);
+        }
+
+        private async Task HelloWorker(string name)
+        {
+            this.OutPut = "MVB wants to greet..";
+            await Task.Delay(1000);
+            this.OutPut = $"{name.ToUpper()}!!";
+            await Task.Delay(1000);
+            this.OutPut = "See you soon!";
+        }
+    }
+}
